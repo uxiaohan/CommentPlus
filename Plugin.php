af124f2306f99ffeca6744163e7cda1e08496e70
Han <1655466387@qq.com>
2021-08-01 19:38:19 +0800

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * 微信推送评论通知 - Plus版
+ * 
+ * @package CommentWechatPlus
+ * @author Han
+ * @version 1.0.0
+ * @link https://www.vvhan.com
+ */
+class CommentWechatPlus_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+
+        Typecho_Plugin::factory('Widget_Feedback')->comment = array('CommentWechatPlus_Plugin', 'sc_send');
+        Typecho_Plugin::factory('Widget_Feedback')->trackback = array('CommentWechatPlus_Plugin', 'sc_send');
+        Typecho_Plugin::factory('Widget_XmlRpc')->pingback = array('CommentWechatPlus_Plugin', 'sc_send');
+
+        return _t('请配置此插件的 Token, 以使您的推送生效');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+    {
+    }
+
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $vvhanRadio = new Typecho_Widget_Helper_Form_Element_Radio('vvhanname', array(0 => 'Plus+', 1 => '酷推'), 0, _t('选择 Plus+ 或者 酷推（推荐PlusPlus）'));
+        $form->addInput($vvhanRadio);
+
+        $key = new Typecho_Widget_Helper_Form_Element_Text('vvhantoken', NULL, NULL, _t('秘钥'), _t('在秘钥处 Plus+填入Plus+的 Token ，酷推填入酷推的 Skey<br />Plus+注册 <a href="https://pushplus.hxtrip.com/">注册</a><br />酷推注册 <a href="https://cp.xuthus.cc/">注册</a><br />
+        注册后需要绑定你的 微信号 或者 QQ号 才能收到推送！'));
+        $form->addInput($key->addRule('required', _t('您必须填写一个正确的 Token')));
+    }
+
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
+
+    /**
+     * 微信推送
+     * 
+     * @access public
+     * @param array $comment 评论结构
+     * @param Typecho_Widget $post 被评论的文章
+     * @return void
+     */
+    public static function sc_send($comment, $post)
+    {
+        $options = Typecho_Widget::widget('Widget_Options');
+        $vvhanToken = $options->plugin('CommentWechatPlus')->vvhantoken;
+        $vvhanOr = !$options->plugin('CommentWechatPlus')->vvhanname;
+        $text = "有人在您的博客发表了评论";
+        if ($vvhanOr && substr($comment['text'], 0, 8) === '{!{data:') {
+            preg_match_all('/data:image(.*?)\}!\}/i', $comment['text'], $out);
+            $conHtmlorImg = "<img src='" . str_replace('}!}', '', $out[0][0]) . "' style='width: 100%;'>";
+        } else {
+            $conHtmlorImg = $comment['text'];
+        }
+        $desp = "<span>文章：</span><a href='{$post->permalink}'
+    style='text-decoration: none;color:cornflowerblue ;'>{$post->title}</a><br><span>用户：{$comment['author']}</span><br><span>评论：</span>{$conHtmlorImg}";
+        $postdata = http_build_query(
+            array(
+                'title' => $text,
+                'content' => $desp,
+                'token' => $vvhanToken
+            )
+        );
+        $opts = array(
+            'http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+        $context  = stream_context_create($opts);
+        $result = $vvhanOr ? file_get_contents('http://pushplus.hxtrip.com/send', false, $context) : file_get_contents('https://push.xuthus.cc/send/' . $vvhanToken . '?c=' . $post->permalink . 'ㅤ被评论ㅤ内容：' . $conHtmlorImg);
+        return  $comment;
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# CommentPlus
+
+> Typecho 插件——支持 Plus+ 和 酷推 微信推送评论通知 - CommentPlus
+
+## 感谢
+
+本插件的微信推送采用了 [@Plus+](https://pushplus.hxtrip.com/) 的 [微信推送](https://pushplus.hxtrip.com/) 解决方案，在此特别感谢 @plus+ ！
+本插件的微信推送采用了 [@酷推](https://cp.xuthus.cc/) 的 [QQ 推送](https://cp.xuthus.cc/) 解决方案，在此特别感谢 @酷推 ！
+
+## 使用方法
+
+1.  [点此下载](https://github.com/uxiaohan/CommentPlus/archive/main.zip)后解压，将解压后的目录名改为 `CommentPlus`，然后上传到你的 Typecho 的 `/usr/plugins`，并在 Typecho 后台开启插件
+
+2.
+
+- 到[Plus+](https://pushplus.hxtrip.com/)里申请你的专属 `Token`，并根据提示绑定你的微信
+- 到[酷推](https://cp.xuthus.cc/)里申请你的专属 `Skey`，并根据提示绑定你的 QQ（只支持 QQ 推送）
+
+3.  将你申请到的 `Token` 或者 `Skey` 填到插件设置的 `密钥` 里，保存即可
+
+## 截图
+
+![截图](demo.png)
+
+## 更新记录
+
+- v1.0(20210801)
+
+  - [+]集成 Plus+ 和 酷推 并移除 收费的 方糖（Server 酱）并完美支持 Typecho-Joe-Theme 的图片评论推送
